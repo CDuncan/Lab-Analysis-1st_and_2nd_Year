73d62a547602af8dffb5d1a10190606635d79e05
Lalichi <lalichi2011@gmail.com>
2017-02-19 01:10:04 +0000

    Update HVError2.m

--- Lens Optics/HVError2.m
+++ Lens Optics/HVError2.m
@@ -9,16 +9,16 @@ function HVError2(X,eX,Y,eY,Xrange)
 terrorbar(X,Y,eY,0.003,'units');
 hold;
 
-XMod	= X(Xrange:1:length(X));
-YMod	= Y(Xrange:1:length(X));
-eYMod	= eY(Xrange:1:length(X));
+XMod		= X(Xrange:1:length(X));
+YMod		= Y(Xrange:1:length(X));
+eYMod		= eY(Xrange:1:length(X));
 herrorbar(XMod,YMod,eYMod,'b');
 
 %Calculate and plot fit
-fit	= polyfit(X,Y,1);
+fit		= polyfit(X,Y,1);
 disp(fit)
 %Find x intercept as an integer
-xint_fp	= -(fit(2)/fit(1));
-x_lim	= floor(xint_fp);
+xint_fp		= -fit(2)/fit(1);
+x_lim		= floor(xint_fp);
 plot(X,[fit(1)*X + fit(2)],'-r','LineWidth',0.05);
 plot([x_lim,0],[fit(1)*[x_lim,0] + fit(2)],'-r','LineWidth',0.05);
