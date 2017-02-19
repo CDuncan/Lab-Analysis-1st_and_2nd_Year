2de6b516261c341fa579c442ba7ca6dabac5653a
Lalichi <lalichi2011@gmail.com>
2017-02-19 01:13:41 +0000

    Update GraphSettings.m

--- settings/GraphSettings.m
+++ settings/GraphSettings.m
@@ -1,30 +1,30 @@
 % Graph Settings
 
 % General
-ax = gca;
-ax.Box = 'off';
-ax.TickDir = 'out';
-ax.TickLength = [0.01 0.035];
+ax			= gca;
+ax.Box			= 'off';
+ax.TickDir		= 'out';
+ax.TickLength		= [0.01 0.035];
 
 % X-Axis
-ax.XGrid = 'on';
-ax.XAxisLocation = 'bottom';
-ax.XMinorGrid = 'on';
-ax.XMinorTick = 'on';
-ax.XLabel.String = 'X Label'
-ax.XLabel.FontSize = 11
-ax.XLabel.FontWeight = 'bold'
+ax.XGrid		= 'on';
+ax.XAxisLocation 	= 'bottom';
+ax.XMinorGrid		= 'on';
+ax.XMinorTick		= 'on';
+ax.XLabel.String	= 'X Label'
+ax.XLabel.FontSize	= 11
+ax.XLabel.FontWeight	= 'bold'
 %~ax.XTick = A:B:C;
 %~ax.XLim = [floor(min(X)) ceil(max(X))];
 
 
 % Y-Axis
-ax.YGrid = 'on';
-ax.YAxisLocation = 'origin';
-ax.YMinorGrid = 'on';
-ax.YMinorTick = 'on';
-ax.YLabel.String = 'Y Label'
-ax.XLabel.FontSize = 11
-ax.YLabel.FontWeight = 'bold'
+ax.YGrid		= 'on';
+ax.YAxisLocation	= 'origin';
+ax.YMinorGrid		= 'on';
+ax.YMinorTick		= 'on';
+ax.YLabel.String	= 'Y Label'
+ax.XLabel.FontSize	= 11
+ax.YLabel.FontWeight	= 'bold'
 %~ax.YTick = A:B:C;
 %~ax.YLim = [floor(min(Y)) ceil(max(Y))];
