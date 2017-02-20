fcf8a090d842238d3ea2f04cd1a85455f169e029
Lalichi <lalichi2011@gmail.com>
2017-02-20 05:11:36 +0000

    Update errorsquare.m

--- functions/errorsquare.m
+++ functions/errorsquare.m
@@ -1,11 +1,11 @@
 function errorsquare(X,eX,Y,eY,Size)
 % For errors in both x and y a rectangle is drawn containing the region of possible values given one SD 
 
-ax = gca;
-hold;
-for A = 1:length(X)
-    pX = [X(A)+eX(A),X(A)-eX(A),X(A)-eX(A),X(A)+eX(A)];
-    pY = [Y(A)+eY(A),Y(A)+eY(A),Y(A)-eY(A),Y(A)-eY(A)];
+ax	= gca;
+hold	='on';
+for A	= 1:length(X)
+    pX	= [X(A)+eX(A),X(A)-eX(A),X(A)-eX(A),X(A)+eX(A)];
+    pY	= [Y(A)+eY(A),Y(A)+eY(A),Y(A)-eY(A),Y(A)-eY(A)];
     patch(pX,pY,'or','FaceAlpha',0.3);
 end
 switch Size
