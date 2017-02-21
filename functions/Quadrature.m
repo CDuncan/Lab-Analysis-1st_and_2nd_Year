ad94e4dc98abaeb7d4d61c628bf9de9af6214db9
Lalichi <lalichi2011@gmail.com>
2017-02-21 14:57:00 +0000

    Create Quadrature.m

---
+++ functions/Quadrature.m
@@ -0,0 +1,23 @@
+function [ output ] = Quadrature(A,B,C,D,E)
+%Combines values in quadrature F^2 = A^2 + B^2 + C^2 + D^2 + E^2
+
+Flag = 0
+switch nargin
+	case 5
+		Fsq	= A.^2 + B.^2 + C.^2 + D.^2 + E.^2;
+	case 4
+		Fsq	= A.^2 + B.^2 + C.^2 + D.^2;
+	case 3
+		Fsq	= A.^2 + B.^2 + C.^2;
+	case 2
+		Fsq	= A.^2 + B.^2;
+	otherwise
+		Flag	= 1;
+end
+
+switch Flag
+	case 0
+		output = Fsq.^0.5;
+	case 1
+		disp('Invalid input');
+end
