8d35ab1b301115fd683eb61d00d4b75d9d79be42
Lalichi <lalichi2011@gmail.com>
2017-02-21 15:02:44 +0000

    Update Quadrature.m

--- functions/Quadrature.m
+++ functions/Quadrature.m
@@ -1,7 +1,6 @@
 function [ output ] = Quadrature(A,B,C,D,E)
 %Combines values in quadrature F^2 = A^2 + B^2 + C^2 + D^2 + E^2
 
-Flag = 0;
 switch nargin
 	case 5
 		Fsq	= A.^2 + B.^2 + C.^2 + D.^2 + E.^2;
@@ -11,13 +10,17 @@ switch nargin
 		Fsq	= A.^2 + B.^2 + C.^2;
 	case 2
 		Fsq	= A.^2 + B.^2;
-	otherwise
-		Flag	= 1;
 end
 
-switch Flag
-	case 0
+switch nargin
+	case {0, 1}
+		diff = 2 - nargin
+		error = sprintf('Needs %.0f more inputs',diff);
+		disp(error);
+	case {2, 3, 4, 5}
 		output = Fsq.^0.5;
-	case 1
-		disp('Needs 2 to 5 inputs');
+	otherwise
+		diff = nargin - 5
+		error = sprintf('Needs %.0f less inputs',diff);
+		disp(error);
 end
