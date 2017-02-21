3bc2c6ad5a17d68ea1896f5536071bd9ada95bfe
Lalichi <lalichi2011@gmail.com>
2017-02-21 15:04:55 +0000

    Update Quadrature.m

--- functions/Quadrature.m
+++ functions/Quadrature.m
@@ -12,15 +12,4 @@ switch nargin
 		Fsq	= A.^2 + B.^2;
 end
 
-switch nargin
-	case {0, 1}
-		diff = 2 - nargin;
-		error = sprintf('Needs %.0f more inputs',diff);
-		disp(error);
-	case {2, 3, 4, 5}
-		output = Fsq.^0.5;
-	otherwise
-		diff = nargin - 5;
-		error = sprintf('Needs %.0f less inputs',diff);
-		disp(error);
-end
+output = Fsq.^0.5;
