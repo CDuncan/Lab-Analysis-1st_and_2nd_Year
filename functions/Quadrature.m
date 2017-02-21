cb176d85cc8549a3f4d17686eabf97ef2dad2eb2
Lalichi <lalichi2011@gmail.com>
2017-02-21 14:57:56 +0000

    Update Quadrature.m

--- functions/Quadrature.m
+++ functions/Quadrature.m
@@ -1,7 +1,7 @@
 function [ output ] = Quadrature(A,B,C,D,E)
 %Combines values in quadrature F^2 = A^2 + B^2 + C^2 + D^2 + E^2
 
-Flag = 0
+Flag = 0;
 switch nargin
 	case 5
 		Fsq	= A.^2 + B.^2 + C.^2 + D.^2 + E.^2;
