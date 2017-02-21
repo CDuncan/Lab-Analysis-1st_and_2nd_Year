f1770189fab5a93d403afce3ad3b516223ed5058
Lalichi <lalichi2011@gmail.com>
2017-02-21 17:01:53 +0000

    Update LimSet.m

--- functions/LimSet.m
+++ functions/LimSet.m
@@ -1,11 +1,12 @@
 function [XLim,YLim] = LimSet(X,intervalX,Y,intervalY)
 % Sets bounds
+
 % Min
 XMin		= min(X);
 XMinRound	= floor(XMin);
 XTest		= XMinRound;
 while XTest<XMin;
-XTest		= XTest + intervalX;
+	XTest	= XTest + intervalX;
 end
 XMin		= XTest-intervalX;
 
@@ -22,9 +23,9 @@ XMax		= XTest+intervalX;
 
 switch nargin
 	case 4
-		[YLim,Place] = LimSet(Y,intervalY);
-    case 2
-        YLim = [0,0];
+		[YLim,Placeholder] = LimSet(Y,intervalY);
+	case 2
+        	YLim = [0,0];
 end
 
 XLim = [XMin,XMax];
